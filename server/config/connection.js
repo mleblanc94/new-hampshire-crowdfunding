a08d44c09b80977c475b2e9ee3edbe5cabdd15b2
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-12-04 21:14:53 -0500

    Created models, Auth.js, Seed files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
+//imports mongoose
 const mongoose = require('mongoose');
-// need the mango url inside ' '
-mongoose.connect(process.env.MONGODB_URI || '');
 
-module.exports = mongoose.connection;
+//connects to mongodb database 'crowdfunding' - if it doesn't exist, it will create it
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crowdfunding');
+
+module.exports = mongoose.connection;//exports connection
