8726fdf88f7f4c4d77bf96fb5bd4ff57cb8277d0
joseph <jomill0723@outlook.com>
2023-12-06 21:12:58 -0500

    updated resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,57 @@
-
-const { User, Project,  } = require('./models');
+const { User, Project } = require('./models');
 
 const resolvers = {
   Query: {
-    getUserById: async (_, { userId }) => {
-      return await User.findById(userId);
+    getProjectById: async (_, { id }) => {
+      return await Project.findById(id);
     },
-    getProjectById: async (_, { projectId }) => {
-      return await Project.findById(projectId);
+    getAllProjects: async () => {
+      return await Project.find();
     },
+    getUserById: async (_, { id }) => {
+      return await User.findById(id);
+    },
+    getAllUsers: async () => {
+      return await User.find();
+    },
+    // Add other query resolvers as needed
   },
   Mutation: {
-    createUser: async (_, { username, email, password }) => {
-      const newUser = new User({ username, email, password });
-      await newUser.save();
+    createProject: async (_, { input }) => {
+      const newProject = await Project.create(input);
+      return newProject;
+    },
+    updateProject: async (_, { id, input }) => {
+      return await Project.findByIdAndUpdate(id, input, { new: true });
+    },
+    deleteProject: async (_, { id }) => {
+      return await Project.findByIdAndDelete(id);
+    },
+    createUser: async (_, { input }) => {
+      const newUser = await User.create(input);
       return newUser;
     },
-    createProject: async (_, { title, description, creator, fundingGoal,  }) => {
-      const newProject = new Project({ title, description, creator, fundingGoal,  });
-      await newProject.save();
-      return newProject;
+    updateUser: async (_, { id, input }) => {
+      return await User.findByIdAndUpdate(id, input, { new: true });
+    },
+    deleteUser: async (_, { id }) => {
+      return await User.findByIdAndDelete(id);
+    },
+    addTointerestedIn: async (_, { projectId, userId }) => {
+      const project = await Project.findByIdAndUpdate(projectId, { $push: { interestedIn: userId } }, { new: true });
+      return project;
+    },
+    removeFrominterestedIn: async (_, { projectId, userId }) => {
+      const project = await Project.findByIdAndUpdate(projectId, { $pull: { interestedIn: userId } }, { new: true });
+      return project;
+    },
+    addBackerToProject: async (_, { projectId, userId }) => {
+      const project = await Project.findByIdAndUpdate(projectId, { $push: { backers: userId } }, { new: true });
+      return project;
+    },
+    removeBackerFromProject: async (_, { projectId, userId }) => {
+      const project = await Project.findByIdAndUpdate(projectId, { $pull: { backers: userId } }, { new: true });
+      return project;
     },
   },
   Project: {
@@ -28,6 +60,9 @@ const resolvers = {
     },
     backers: async (parent) => {
       return await User.find({ _id: { $in: parent.backers } });
+    },
+    interestedIn: async (parent) => {
+      return await User.find({ _id: { $in: parent.interestedIn } });
     },
   },
 };
