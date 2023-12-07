61eea6bdb1180953aae7f8c646d43969c3690f4d
chris sully <chris.sullivan31@outlook.com>
2023-12-06 19:13:35 -0500

    typeDefs

--- server/server.js
+++ server/server.js
@@ -21,7 +21,6 @@ const startApolloServer = async () => {
   
   app.use('/graphql', expressMiddleware(server));
 
-  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
