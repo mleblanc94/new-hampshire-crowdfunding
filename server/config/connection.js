f86adcedc9762654b4eaae6eff1ce2a51865f3f8
Michael Leblanc <mleblanc1@werfen.com>
2023-12-11 23:16:00 -0500

    adding changes I made in connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 //imports mongoose
 const mongoose = require('mongoose');
 
-//connects to mongodb database 'crowdfunding' - if it doesn't exist, it will create it| added mongoDB URI
+//connects to mongodb database 'crowdfunding' - if it doesn't exist, it will create it| added mongoDBURI
 const mongoDBUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crowdfunding';
 mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
