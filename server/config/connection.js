52fcb26a1a094c48123cdcdbe481703a1ed530b9
Michael Leblanc <mleblanc1@werfen.com>
2023-12-11 22:17:34 -0500

    changed db

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 
 //connects to mongodb database 'crowdfunding' - if it doesn't exist, it will create it
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crowdfunding');
+mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose.connection;//exports connection
